db22a37965f0b7fbd2980a63c5085927855e01fe
atkinsonholly <atkinsonholly@gmail.com>
2018-11-22 16:09:31 +0000

    Tests passing

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Dog
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def self.all
+    @@all
+    @@all.each {|dog| puts dog.name}
+  end
+
+
+end
